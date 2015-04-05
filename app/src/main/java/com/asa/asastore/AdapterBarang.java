3c9af8da786bd1f1ee17503c24a822c330e91a0d
Aprijal Pasaribu <aprijal.psrb@gmail.com>
2015-04-05 17:04:51 +0700

    Ubah nama class

---
+++ app/src/main/java/com/asa/asastore/AdapterBarang.java
@@ -0,0 +1,47 @@
+package com.asa.asastore;
+
+import android.content.Context;
+import android.graphics.Color;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by APRIJAL_PASARIBU on 03/04/2015.
+ */
+public class AdapterBarang extends ArrayAdapter<DataBarang> {
+
+    public AdapterBarang(Context context, int resource, List<DataBarang> objects) {
+        super(context, resource, objects);
+    }
+
+    @Override
+    public View getView(int position,View view, ViewGroup parent){
+        if(view == null){
+            LayoutInflater vi;
+            vi = LayoutInflater.from(getContext());
+            view = vi.inflate(R.layout.list_item_home,null);
+        }
+        DataBarang dataBarang = getItem(position);
+        if(dataBarang != null){
+            TextView productName = (TextView)view.findViewById(R.id.text_productName);
+            TextView productPrice = (TextView)view.findViewById(R.id.text_product_price);
+            TextView productDate = (TextView)view.findViewById(R.id.text_product_stock_date);
+            TextView productCategory = (TextView)view.findViewById(R.id.text_product_category);
+            ImageView imageView = (ImageView)view.findViewById(R.id.image_status_favorite);
+            productName.setText(dataBarang.getNama_barang());
+            productPrice.setText("Rp "+dataBarang.getHarga_barang());
+            productDate.setText(dataBarang.getTgl_harga_stok_barang());
+            productCategory.setText(dataBarang.getKategori_barang());
+            imageView.setBackgroundColor(Color.parseColor(dataBarang.getWarna_favorite()));
+        }
+        return view;
+    }
+}
