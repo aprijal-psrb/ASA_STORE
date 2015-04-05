efa32d7ae3d19c8b78a714dbb7b3116f5489f32e
Aprijal Pasaribu <aprijal.psrb@gmail.com>
2015-04-05 15:53:51 +0700

    Memperbaharui AdapterHomeBarang untuk DataBarang

--- app/src/main/java/com/asa/asastore/AdapterHomeBarang.java
+++ app/src/main/java/com/asa/asastore/AdapterHomeBarang.java
@@ -16,8 +16,8 @@ import java.util.List;
 /**
  * Created by APRIJAL_PASARIBU on 03/04/2015.
  */
-public class AdapterHomeBarang extends ArrayAdapter<ArrayList<String>> {
-    private ArrayList<String> list;
+public class AdapterHomeBarang extends ArrayAdapter<DataBarang> {
+    private DataBarang dataBarang;
 
 
     public AdapterHomeBarang(Context context, int resource) {
@@ -28,19 +28,19 @@ public class AdapterHomeBarang extends ArrayAdapter<ArrayList<String>> {
         super(context, resource, textViewResourceId);
     }
 
-    public AdapterHomeBarang(Context context, int resource, ArrayList<String>[] objects) {
+    public AdapterHomeBarang(Context context, int resource, DataBarang[] objects) {
         super(context, resource, objects);
     }
 
-    public AdapterHomeBarang(Context context, int resource, int textViewResourceId, ArrayList<String>[] objects) {
+    public AdapterHomeBarang(Context context, int resource, int textViewResourceId, DataBarang[] objects) {
         super(context, resource, textViewResourceId, objects);
     }
 
-    public AdapterHomeBarang(Context context, int resource, List<ArrayList<String>> objects) {
+    public AdapterHomeBarang(Context context, int resource, List<DataBarang> objects) {
         super(context, resource, objects);
     }
 
-    public AdapterHomeBarang(Context context, int resource, int textViewResourceId, List<ArrayList<String>> objects) {
+    public AdapterHomeBarang(Context context, int resource, int textViewResourceId, List<DataBarang> objects) {
         super(context, resource, textViewResourceId, objects);
     }
 
@@ -51,26 +51,26 @@ public class AdapterHomeBarang extends ArrayAdapter<ArrayList<String>> {
             vi = LayoutInflater.from(getContext());
             view = vi.inflate(R.layout.list_item_home,null);
         }
-        list = getItem(position);
-        if(list != null){
+        dataBarang = getItem(position);
+        if(dataBarang != null){
             TextView productName = (TextView)view.findViewById(R.id.text_productName);
             TextView productPrice = (TextView)view.findViewById(R.id.text_product_price);
             TextView productDate = (TextView)view.findViewById(R.id.text_product_stock_date);
             TextView productCategory = (TextView)view.findViewById(R.id.text_product_category);
             ImageView imageView = (ImageView)view.findViewById(R.id.image_status_favorite);
-            productName.setText(list.get(5));
-            productPrice.setText("Rp "+list.get(8));
-            productDate.setText(list.get(9));
-            productCategory.setText(list.get(12));
+            productName.setText(dataBarang.getNama_barang());
+            productPrice.setText("Rp "+dataBarang.getHarga_barang());
+            productDate.setText(dataBarang.getTgl_harga_stok_barang());
+            productCategory.setText(dataBarang.getKategori_barang());
             imageView.setBackgroundColor(Color.parseColor(getColorFavorite()));
         }
         return view;
     }
 
     public String getColorFavorite(){
-        for(int i = 0; i < MainActivity.listFavoriteCategory.size(); i++){
-            if(list.get(14).equals(MainActivity.listFavoriteCategory.get(i).get(0))){
-                return MainActivity.listFavoriteCategory.get(i).get(1);
+        for(int i = 0; i < MainActivity.listDataFavorite.size(); i++){
+            if(dataBarang.getId_favorite().equals(MainActivity.listDataFavorite.get(i).getId_favorite())){
+                return MainActivity.listDataFavorite.get(i).getWarna_favorite();
             }
         }
         return "#00000000";
