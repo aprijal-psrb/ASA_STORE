097d8202d241e77f161bc626a87b2cd48f2a4df8
Aprijal Pasaribu <aprijal.psrb@gmail.com>
2015-04-05 16:07:56 +0700

    Menghapus kode - kode yg tidak perlu

--- app/src/main/java/com/asa/asastore/MainActivity.java
+++ app/src/main/java/com/asa/asastore/MainActivity.java
@@ -3,7 +3,6 @@ package com.asa.asastore;
 import android.app.ActionBar;
 import android.app.AlertDialog;
 import android.app.FragmentTransaction;
-import android.app.ProgressDialog;
 import android.content.DialogInterface;
 import android.os.AsyncTask;
 import android.support.v4.app.Fragment;
@@ -12,39 +11,17 @@ import android.os.Bundle;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentStatePagerAdapter;
 import android.support.v4.view.ViewPager;
-import android.util.Log;
-import android.view.ContextMenu;
-import android.view.LayoutInflater;
 import android.view.Menu;
-import android.view.MenuInflater;
 import android.view.MenuItem;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
-import android.widget.ListView;
-import android.widget.TextView;
-import android.widget.Toast;
-
 import org.apache.http.NameValuePair;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
-
-import java.nio.channels.ConnectionPendingException;
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
 
-
 public class MainActivity extends FragmentActivity implements ViewPager.OnPageChangeListener,ActionBar.TabListener{
-    public static ArrayList<ArrayList<String>> allBarang = new ArrayList<>();
-    public static ArrayList<ArrayList<String>> allPenjual = new ArrayList<>();
-    public static ArrayList<ArrayList<String>> allFavorite = new ArrayList<>();
-    public static ArrayList<ArrayList<String>> allMerek = new ArrayList<>();
-    public static ArrayList<ArrayList<String>> listHomeBarang = new ArrayList<>();
-    public static ArrayList<ArrayList<String>> listShoppingSupplier = new ArrayList<>();
-    public static ArrayList<ArrayList<String>> listFavoriteCategory = new ArrayList<>();
+    public static JSONParser jsonParser = new JSONParser();
     public static AdapterHomeBarang adapterHomeBarang;
     public static AdapterShoppingSupplier adapterShoppingSupplier;
     public static AdapterFavoriteCategory adapterFavoriteCategory;
@@ -55,9 +32,6 @@ public class MainActivity extends FragmentActivity implements ViewPager.OnPageCh
     MyPagerAdapter mPagerAdapter;
     ViewPager mViewPager;
     ActionBar actionBar;
-    ProgressDialog pDialog;
-    public static JSONParser jsonParser = new JSONParser();
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -72,122 +46,46 @@ public class MainActivity extends FragmentActivity implements ViewPager.OnPageCh
         actionBar.addTab(actionBar.newTab().setIcon(R.drawable.home_status).setTabListener(this));
         actionBar.addTab(actionBar.newTab().setIcon(R.drawable.belanja_status).setTabListener(this));
         actionBar.addTab(actionBar.newTab().setIcon(R.drawable.favorite_status).setTabListener(this));
-
     }
-
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
     }
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
         switch (id){
             case R.id.refresh:
                 new GetData().execute();
                 return true;
-
         }
-
         return super.onOptionsItemSelected(item);
     }
-
-    /**
-     * This method will be invoked when the current page is scrolled, either as part
-     * of a programmatically initiated smooth scroll or a user initiated touch scroll.
-     *
-     * @param position             Position index of the first page currently being displayed.
-     *                             Page position+1 will be visible if positionOffset is nonzero.
-     * @param positionOffset       Value from [0, 1) indicating the offset from the page at position.
-     * @param positionOffsetPixels Value in pixels indicating the offset from position.
-     */
     @Override
     public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
-
     }
-
-    /**
-     * This method will be invoked when a new page becomes selected. Animation is not
-     * necessarily complete.
-     *
-     * @param position Position index of the new selected page.
-     */
     @Override
     public void onPageSelected(int position) {
         getActionBar().setSelectedNavigationItem(position);
     }
-
-    /**
-     * Called when the scroll state changes. Useful for discovering when the user
-     * begins dragging, when the pager is automatically settling to the current page,
-     * or when it is fully stopped/idle.
-     *
-     * @param state The new scroll state.
-     * @see android.support.v4.view.ViewPager#SCROLL_STATE_IDLE
-     * @see android.support.v4.view.ViewPager#SCROLL_STATE_DRAGGING
-     * @see android.support.v4.view.ViewPager#SCROLL_STATE_SETTLING
-     */
     @Override
     public void onPageScrollStateChanged(int state) {
-
     }
-
-    /**
-     * Called when a tab enters the selected state.
-     *
-     * @param tab The tab that was selected
-     * @param ft  A {@link android.app.FragmentTransaction} for queuing fragment operations to execute
-     *            during a tab switch. The previous tab's unselect and this tab's select will be
-     *            executed in a single transaction. This FragmentTransaction does not support
-     */
     @Override
     public void onTabSelected(ActionBar.Tab tab, FragmentTransaction ft) {
         mViewPager.setCurrentItem(tab.getPosition());
     }
-
-    /**
-     * Called when a tab exits the selected state.
-     *
-     * @param tab The tab that was unselected
-     * @param ft  A {@link android.app.FragmentTransaction} for queuing fragment operations to execute
-     *            during a tab switch. This tab's unselect and the newly selected tab's select
-     *            will be executed in a single transaction. This FragmentTransaction does not
-     */
     @Override
     public void onTabUnselected(ActionBar.Tab tab, FragmentTransaction ft) {
-
     }
-
-    /**
-     * Called when a tab that is already selected is chosen again by the user.
-     * Some applications may use this action to return to the top level of a category.
-     *
-     * @param tab The tab that was reselected.
-     * @param ft  A {@link android.app.FragmentTransaction} for queuing fragment operations to execute
-     *            once this method returns. This FragmentTransaction does not support
-     */
     @Override
     public void onTabReselected(ActionBar.Tab tab, FragmentTransaction ft) {
-
     }
-
     public class MyPagerAdapter extends FragmentStatePagerAdapter {
         public MyPagerAdapter(FragmentManager fm) {
             super(fm);
         }
-        Fragment mFragment;
-        /**
-         * Return the Fragment associated with a specified position.
-         *
-         * @param i
-         */
         @Override
         public Fragment getItem(int i) {
             switch (i){
@@ -200,37 +98,20 @@ public class MainActivity extends FragmentActivity implements ViewPager.OnPageCh
             }
             return null;
         }
-
-        /**
-         * Return the number of views available.
-         */
         @Override
         public int getCount() {
             return 3;
         }
-
         @Override
         public CharSequence getPageTitle(int position) {
             return "OBJECT " + (position + 1);
         }
-
     }
-    public void getData(){
-        new GetData().execute();
-    }
-    public static int TAG = 0;
     public class GetData extends AsyncTask<String,Integer,Integer>{
         int success;
         @Override
         protected void onPreExecute(){
             super.onPreExecute();
-            //if(TAG == 0) {
-            //    pDialog = new ProgressDialog(MainActivity.this);
-            //    pDialog.setMessage("Please Wait");
-            //    pDialog.setIndeterminate(false);
-            //    pDialog.setCancelable(false);
-            //    pDialog.show();
-            //}
         }
         @Override
         protected Integer doInBackground(String... params) {
@@ -246,8 +127,6 @@ public class MainActivity extends FragmentActivity implements ViewPager.OnPageCh
                 success = jsonObject.getInt("success");
                 if (success == 1){
                     JSONArray all_barang = jsonObject.getJSONArray("all_barang");
-                    //allBarang.clear();
-                    //listHomeBarang.clear();
                     listDataBarang.clear();
                     for(int n = 0; n < all_barang.length(); n++){
                         JSONObject c = all_barang.getJSONObject(n);
@@ -266,26 +145,6 @@ public class MainActivity extends FragmentActivity implements ViewPager.OnPageCh
                         String kategori_barang = c.getString("kategori_barang");
                         String deskripsi_barang = c.getString("deskripsi_barang");
                         String id_favorite = c.getString("id_favorite");
-                        /*
-                        ArrayList<String> arrayList = new ArrayList<>();
-                        arrayList.add(id_barang);
-                        arrayList.add(id_user);
-                        arrayList.add(id_merek);
-                        arrayList.add(id_penjual);
-                        arrayList.add(id_gambar);
-                        arrayList.add(nama_barang);
-                        arrayList.add(stok_barang);
-                        arrayList.add(satuan_barang);
-                        arrayList.add(harga_barang);
-                        arrayList.add(tgl_harga_stok_barang);
-                        arrayList.add(kode_barang);
-                        arrayList.add(lokasi_barang);
-                        arrayList.add(kategori_barang);
-                        arrayList.add(deskripsi_barang);
-                        arrayList.add(id_favorite);
-                        allBarang.add(arrayList);
-                        */
-                        //listHomeBarang.add(arrayList);
                         DataBarang dataBarang = new DataBarang();
                         dataBarang.setId_barang(id_barang);
                         dataBarang.setId_user(id_user);
@@ -312,15 +171,12 @@ public class MainActivity extends FragmentActivity implements ViewPager.OnPageCh
             }catch (NullPointerException e){
                 e.printStackTrace();
             }
-
             all = new ArrayList<>();
             jsonObject = jsonParser.makeHttpRequest("http://192.168.173.1/asa/asastore/get-penjual.php","GET",all);
             try{
                 int success = jsonObject.getInt("success");
                 if (success == 1){
                     JSONArray penjual = jsonObject.getJSONArray("penjual");
-                    //allPenjual.clear();
-                    //listShoppingSupplier.clear();
                     listDataSupplier.clear();
                     for(int n = 0; n < penjual.length(); n++){
                         JSONObject c = penjual.getJSONObject(n);
@@ -331,18 +187,6 @@ public class MainActivity extends FragmentActivity implements ViewPager.OnPageCh
                         String geolocation = c.getString("geolocation");
                         String kontak_toko = c.getString("kontak_toko");
                         String email_toko = c.getString("email_toko");
-                        /*
-                        ArrayList<String> arrayList = new ArrayList<>();
-                        arrayList.add(id_penjual);
-                        arrayList.add(nama_penjual);
-                        arrayList.add(nama_toko);
-                        arrayList.add(alamat_toko);
-                        arrayList.add(geolocation);
-                        arrayList.add(kontak_toko);
-                        arrayList.add(email_toko);
-                        allPenjual.add(arrayList);
-                        listShoppingSupplier.add(arrayList);
-                        */
                         DataSupplier dataSupplier = new DataSupplier();
                         dataSupplier.setId_penjual(id_penjual);
                         dataSupplier.setNama_penjual(nama_penjual);
@@ -359,20 +203,15 @@ public class MainActivity extends FragmentActivity implements ViewPager.OnPageCh
             }catch (JSONException e){
                 e.printStackTrace();
             }
-
             all = new ArrayList<>();
             jsonObject = jsonParser.makeHttpRequest("http://192.168.173.1/asa/asastore/get-favorite.php","GET",all);
             if(jsonObject == null){
                 return 1;
             }
-
             try{
                 int success = jsonObject.getInt("success");
                 if (success == 1){
                     JSONArray favorite = jsonObject.getJSONArray("favorite");
-                    //allFavorite.clear();
-                    //listFavoriteCategory.clear();
-                    //Favorite.listCategory = new ArrayList<>();
                     listDataFavorite.clear();
                     for(int n = 0; n < favorite.length(); n++){
                         JSONObject c = favorite.getJSONObject(n);
@@ -380,20 +219,6 @@ public class MainActivity extends FragmentActivity implements ViewPager.OnPageCh
                         String warna_favorite = c.getString("warna_favorite");
                         String nama_favorite = c.getString("nama_favorite");
                         String deskripsi = c.getString("deskripsi");
-                        /*
-                        ArrayList<String> arrayList = new ArrayList<>();
-                        arrayList.add(id_favorite);
-                        arrayList.add(warna_favorite);
-                        arrayList.add(nama_favorite);
-                        arrayList.add(deskripsi);
-                        allFavorite.add(arrayList);
-                        FavoriteCategory temp = new FavoriteCategory();
-                        temp.setCategoryName(nama_favorite);
-                        temp.setCategoryColor(warna_favorite);
-                        //Favorite.listCategory.add(temp);
-                        listFavoriteCategory.add(arrayList);
-                        color.put(id_favorite,warna_favorite);
-                        */
                         DataFavorite dataFavorite = new DataFavorite();
                         dataFavorite.setId_favorite(id_favorite);
                         dataFavorite.setWarna_favorite(warna_favorite);
@@ -407,18 +232,15 @@ public class MainActivity extends FragmentActivity implements ViewPager.OnPageCh
             }catch (JSONException e){
                 e.printStackTrace();
             }
-
             all = new ArrayList<>();
             jsonObject = jsonParser.makeHttpRequest("http://192.168.173.1/asa/asastore/get-merek.php","GET",all);
             if(jsonObject == null){
                 return 1;
             }
-
             try{
                 int success = jsonObject.getInt("success");
                 if (success == 1){
                     JSONArray merek = jsonObject.getJSONArray("merek");
-                    //allMerek.clear();
                     listDataMerek.clear();
                     for(int n = 0; n < merek.length(); n++){
                         JSONObject c = merek.getJSONObject(n);
@@ -426,14 +248,6 @@ public class MainActivity extends FragmentActivity implements ViewPager.OnPageCh
                         String nama_merek = c.getString("nama_merek");
                         String logo_merek = c.getString("logo_merek");
                         String deskripsi_merek = c.getString("deskripsi_merek");
-                        /*
-                        ArrayList<String> arrayList = new ArrayList<>();
-                        arrayList.add(id_merek);
-                        arrayList.add(nama_merek);
-                        arrayList.add(logo_merek);
-                        arrayList.add(deskripsi_merek);
-                        allMerek.add(arrayList);
-                        */
                         DataMerek dataMerek = new DataMerek();
                         dataMerek.setId_merek(id_merek);
                         dataMerek.setNama_merek(nama_merek);
@@ -467,52 +281,13 @@ public class MainActivity extends FragmentActivity implements ViewPager.OnPageCh
                 }).show();
             }
             if(arg == 0){
-/*
-                Home.arrayListBarang = new ArrayList<>();
-                for (int i = 0; i < MainActivity.allBarang.size(); i++){
-                    Home.arrayListBarang.add(MainActivity.allBarang.get(i).get(5).toString());
-                }
-                //Home.arrayAdapterBarang = new ArrayAdapter(MainActivity.this,android.R.layout.simple_list_item_1,Home.arrayListBarang);
-                adapterBarangHome = new HomeBarangAdapter(MainActivity.this,R.layout.list_item_home,listBarang);
-                //Home.listViewBarang.setAdapter(Home.arrayAdapterBarang);
-                Home.listViewBarang.setAdapter(adapterBarangHome);
-                Shopping.arrayListSupplier = new ArrayList<>();
-                for (int i = 0; i < MainActivity.allPenjual.size(); i++){
-                    Shopping.arrayListSupplier.add(MainActivity.allPenjual.get(i).get(2).toString());
-                }
-                Shopping.arrayAdapterPenjual = new ArrayAdapter(MainActivity.this,android.R.layout.simple_list_item_1,Shopping.arrayListSupplier);
-                //Shopping.listViewSupplier.setAdapter(Shopping.arrayAdapterPenjual);/*
-                Favorite.arrayListCategory = new ArrayList<>();
-                for (int i = 0; i < MainActivity.allFavorite.size(); i++){
-                    Favorite.arrayListCategory.add(MainActivity.allFavorite.get(i).get(2).toString());
-                }
-                //Favorite.arrayAdapterCategory = new ArrayAdapter(MainActivity.this,android.R.layout.simple_list_item_1,Favorite.arrayListCategory);
-                //Favorite.listViewCategory.setAdapter(Favorite.arrayAdapterCategory);   <<<<<   Error!
-                TAG = 1;
-                //pDialog.dismiss();
-*/
                 adapterHomeBarang = new AdapterHomeBarang(MainActivity.this,R.id.layout_item_home,listDataBarang);
                 Home.listViewBarang.setAdapter(adapterHomeBarang);
-
                 adapterShoppingSupplier = new AdapterShoppingSupplier(MainActivity.this,android.R.layout.simple_list_item_1,listDataSupplier);
                 Shopping.listViewSupplier.setAdapter(adapterShoppingSupplier);
-
                 adapterFavoriteCategory = new AdapterFavoriteCategory(MainActivity.this,R.layout.list_item_favorite_category,listDataFavorite);
-
-
             }
-            /*
-            else if(arg == 2) {
-                Home.adapterBarangHome.notifyDataSetChanged();
-                Shopping.arrayAdapterPenjual.notifyDataSetChanged();
-                Favorite.arrayAdapterCategory.notifyDataSetChanged();
-                //pDialog.dismiss();
-            }else{
-                //pDialog.dismiss();
-            }
-            */
         }
     }
-
 }
 
